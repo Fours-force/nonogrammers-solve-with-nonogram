c973f8eafa3f48f1ef5221265b3455c538a7da07
JasonYesBro <spdlqjqlwjs@naver.com>
2023-10-14 20:30:55 +0900

    build: mvc 패키지 yml 생성

--- src/main/java/com/dottree/nonogrammers/NonogrammersApplication.java
+++ src/main/java/com/dottree/nonogrammers/NonogrammersApplication.java
@@ -1,11 +1,14 @@
 package com.dottree.nonogrammers;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude= DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.dottree.nonogrammers", "templates"})
+@MapperScan(value={"com.dottree.nonogrammers.dao"})
 public class NonogrammersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NonogrammersApplication.class, args);
 	}
