1c11408aa52a4d570a0526d04f08568373450a52
JasonYesBro <spdlqjqlwjs@naver.com>
2023-10-14 19:56:38 +0900

    init: 프로젝트 생성 및 ignore 설정

---
+++ src/main/java/com/dottree/nonogrammers/NonogrammersApplication.java
@@ -0,0 +1,13 @@
+package com.dottree.nonogrammers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonogrammersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonogrammersApplication.class, args);
+	}
+
+}
