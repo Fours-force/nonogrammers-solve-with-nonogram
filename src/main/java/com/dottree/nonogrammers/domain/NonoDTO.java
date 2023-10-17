f0d672a1f3d8a261795797acdc6f122640722f78
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-10-18 08:34:55 +0900

    feat: NonoDTO 다시 생성

--- src/main/java/com/dottree/nonogrammers/domain/NonoDTO.java
+++ src/main/java/com/dottree/nonogrammers/domain/NonoDTO.java
@@ -1,15 +1,8 @@
 package com.dottree.nonogrammers.domain;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class NonoDTO {
+    private int nonoId;
     private int levelType;
     private String nonoImgUrl;
     private String rowUrl;
 }
-
-
