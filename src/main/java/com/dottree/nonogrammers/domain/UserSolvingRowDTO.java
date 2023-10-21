37c00626ebe8e23ad057d7ace7b022a73540a6d4
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-10-21 16:14:32 +0900

    feat : UserSolvedCountDTO 추가,UserSolvingRowDTO 추가

---
+++ src/main/java/com/dottree/nonogrammers/domain/UserSolvingRowDTO.java
@@ -0,0 +1,12 @@
+package com.dottree.nonogrammers.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSolvingRowDTO {
+    private int userId;
+    private int nonoId;
+    private int solvingRow;
+}
