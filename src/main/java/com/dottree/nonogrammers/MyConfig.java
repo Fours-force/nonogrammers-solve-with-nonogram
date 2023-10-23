d000fd80a3f289839c7b8bbe7a3f996c84833b06
JasonYesBro <spdlqjqlwjs@naver.com>
2023-10-23 19:40:24 +0900

    update: MyConfig

--- src/main/java/com/dottree/nonogrammers/MyConfig.java
+++ src/main/java/com/dottree/nonogrammers/MyConfig.java
@@ -10,7 +10,9 @@ public class MyConfig implements WebMvcConfigurer {
     String currentDirectory = System.getProperty("user.dir");
     // String location="file:///c:/kosastudy/springedu/src/main/resources/static/images/";
 //    String correctedPath = currentDirectory.re"file:~/Downloads/nonogrammers/src/main/resources/static/images/1/";place("\\", "/");
-    String location = "file:///nonogrammers-solve-with-nonogram/src/main/resources/static/images/profile/";
+//    String location = "file:///nonogrammers-solve-with-nonogram/src/main/resources/static/images/profile/";
+    String location = "file:/Users/jasonmilian/Downloads/nonogrammers/src/main/resources/static/images/profile/";
+
 //"file:~/Downloads/nonogrammers-solve-with-nonogram/src/main/resources/static/images/1/";
 //    " + correctedPath + "/uni-pet/src/main/resources/static/img/mypage/upload/
     @Override
