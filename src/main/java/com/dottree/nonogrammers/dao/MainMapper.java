6209240d8a28d540c9f0e75f0bdc6bb82e841232
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-10-29 11:32:49 +0900

    feat: SCRUM-131 마지막 페이지 확인용 코드 추가

--- src/main/java/com/dottree/nonogrammers/dao/MainMapper.java
+++ src/main/java/com/dottree/nonogrammers/dao/MainMapper.java
@@ -84,9 +84,15 @@ public interface MainMapper {
     @Select("select solvedCount from user_solved_count where userId = #{userId}")
     public Integer selectUserSolvedCount(UserDotDTO udDTO);
 
+    // nono에서 레벨 별 노노들 조회
     @Select("SELECT nonoId, nonoImgUrl, levelType from nono where levelType = #{levelType}")
     public List<UserNonoVO> selectNonoByLevel(@Param("levelType")int levelType);
 
+    // nono에서 모든 노노 조회
     @Select("SELECT nonoId, nonoImgUrl, levelType from nono")
     public List<UserNonoVO> selectAllNoNo();
+
+    //nono의 개수 조회 / 마지막 페이지 확인용
+    @Select("select count(nonoId) from nono")
+    public int selectnonoCount();
 }
