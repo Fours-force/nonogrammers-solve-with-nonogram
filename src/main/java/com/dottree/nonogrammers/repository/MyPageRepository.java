17edfc2894aa6820cbeab9f03cd721c92fd828bf
JasonYesBro <spdlqjqlwjs@naver.com>
2023-11-14 18:38:16 +0900

    refactor: [SCRUM-159] mybatis to JPA repo

--- src/main/java/com/dottree/nonogrammers/repository/MyPageRepository.java
+++ src/main/java/com/dottree/nonogrammers/repository/MyPageRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MyPageRepository extends JpaRepository<User, Long> {
+public interface MyPageRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByNickName(String nickName);
 }
