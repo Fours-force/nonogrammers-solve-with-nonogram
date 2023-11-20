e8f723afa4321ba1bb2d4adc145b513181029f62
JasonYesBro <spdlqjqlwjs@naver.com>
2023-11-20 15:45:59 +0900

    fix: [SCRUM-159] myconfig

--- src/main/java/com/dottree/nonogrammers/MyConfig.java
+++ src/main/java/com/dottree/nonogrammers/MyConfig.java
@@ -1,5 +1,6 @@
 package com.dottree.nonogrammers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,12 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class MyConfig implements WebMvcConfigurer {
     String currentDirectory = System.getProperty("user.dir");
     String location = "file:" + currentDirectory + "/src/main/resources/static/images/";
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         System.out.println("=================== : " +location);
-        registry.addResourceHandler("/images/**").addResourceLocations(location);
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations(location);
     }
 }
