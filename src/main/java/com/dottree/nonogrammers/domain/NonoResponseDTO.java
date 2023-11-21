184784e9cded638dd8d93576baaa6331a9018dbe
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-11-21 18:25:41 +0900

    feat: [SCRUM-115] NoArgsConstructor생성

--- src/main/java/com/dottree/nonogrammers/domain/NonoResponseDTO.java
+++ src/main/java/com/dottree/nonogrammers/domain/NonoResponseDTO.java
@@ -2,10 +2,12 @@ package com.dottree.nonogrammers.domain;
 
 import com.dottree.nonogrammers.entity.Nono;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class NonoResponseDTO {
     private int nonoId;
     private String nonoImgUrl;
