38d67024c7cf35c9f3214212599098876923aa4f
DESKTOP-TH36TT5\COM <babysoo123@gmail.com>
2023-11-23 10:09:31 +0900

    refactor: 추가 /post/wirte

--- src/main/java/com/dottree/nonogrammers/config/SpringSecurityConfig.java
+++ src/main/java/com/dottree/nonogrammers/config/SpringSecurityConfig.java
@@ -35,6 +35,11 @@ public class SpringSecurityConfig {
 
     @Autowired
     private JwtProperties jwtProperties;
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/images/**");
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -63,7 +68,7 @@ public class SpringSecurityConfig {
                                 "api/v1/auth/check-nickname", "api/v1/auth/check-baekjoon",
                                 "/api/v1/auth/email-verification", "/api/v1/auth/verify-code",
                                 "/post", "/post/free", "/post/qa", "/post/nono", "/post/notice",
-                                "/search", "/detail").permitAll()
+                                "/search", "/detail","/post/all","/post/write").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
